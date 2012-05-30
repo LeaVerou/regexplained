6fb6c959b78114e2b0ec97d26d4a2fc43824f5a2
Lea Verou <lea@verou.me>
2012-05-29 23:48:22 -0700

    Initial commit

---
+++ regex-test.js
@@ -0,0 +1,352 @@
+// Code for simple regex testers
+(function(){
+
+var _ = self.RegExpTester = function(container){
+	var me = this;
+	
+	var patternAttr = container.getAttribute('data-pattern'),
+	    initialPattern = RegExp(patternAttr || ''),
+	    initialTest = container.getAttribute('data-test');
+	
+	this.pattern = RegExp(initialPattern.source); 
+	this.flags = container.getAttribute('data-flags') || 'g';
+	this.detailed = !container.hasAttribute('data-simple');
+	this.matches = [];
+	
+	container.classList.add('regex-test');
+	
+	this.input = $u.element.create({
+			tag: 'input',
+			properties: {
+				value: patternAttr,
+				tabIndex: 1
+			}
+		});
+	
+	this.tester = $u.element.create({
+		    	tag: 'input',
+		    	properties: {
+		    		value: initialTest,
+		    		tabIndex: 2
+		    	}
+		    });
+		    
+	this.flagsContainer = $u.element.create('span', this.flags);
+	    
+	$u.element.contents(container, [{
+			tag: 'div',
+			properties: {
+				className: 'pattern'
+			},
+			contents: ['/', {
+				tag: 'div',
+				contents: this.input
+			}, '/', this.flagsContainer]
+		}, {
+			tag: 'br'
+		}, {
+			tag: 'div',
+			properties: {
+				className: 'tester' + (initialPattern.test(initialTest)? '' : ' invalid')
+			},
+	    	contents: ['"', {
+	    		tag: 'div',
+	    		contents: this.tester
+	    	}, '"']
+		}
+	], 'start');
+	
+	
+	this.matchIndicator = $u.element.create('div', {
+		properties: {
+			className: 'match indicator'
+		},
+		inside: this.tester.parentNode
+	});
+	
+	this.submatchIndicator = $u.element.create('div', {
+		properties: {
+			className: 'sub match indicator'
+		},
+		inside: this.tester.parentNode
+	});
+	
+	this.matchIndicator.style.display = this.submatchIndicator.style.display = 'none';
+	
+	container.addEventListener('keydown', function(evt) {
+		if (evt.keyCode === 38 || evt.keyCode === 40) {
+			evt.stopPropagation();
+			evt.preventDefault();
+			
+			me[(evt.keyCode === 40? 'next' : 'prev') + 'Match']();
+		}
+		
+		if (evt.ctrlKey) {
+			if (evt.keyCode === 37 || evt.keyCode === 39) {
+				evt.stopPropagation();
+				evt.preventDefault();
+				
+				me[(evt.keyCode === 39? 'next' : 'prev') + 'Subpattern']();
+			}
+			else if (evt.keyCode === 73) { // I
+				me.toggleFlag('i');
+			}
+			else if (evt.keyCode === 77) { // M
+				me.toggleFlag('m');
+			}
+		}
+	});
+	
+	$u.event.bind([this.input, this.tester], 'input', function(){
+		var div = this.parentNode.parentNode;
+		    
+		div.style.fontSize = _.fontSize(this.value.length) + '%';
+
+		this.style.width = _.getCh(this);
+		
+		me.test();
+	});
+	
+	if(container.classList.contains('slide')) {
+		addEventListener('hashchange', function(){
+			if(container.id === location.hash.slice(1)) {
+				$u.event.fire([me.input, me.tester], 'input');
+			}
+		});
+	}
+	else {
+		$u.event.fire([this.input, this.tester], 'input');
+	}
+};
+
+_.supportsCh = (function() {
+	var dummy = document.createElement('_');
+	dummy.style.width = '1ch';
+	return !!dummy.style.width;
+})();
+
+if(_.supportsCh) {
+	_.getCh = function(input) { return (input.value.length || .2) + 'ch'; }
+}
+else {
+	_.getCh = function(input) {
+		var parent = input.parentNode, dummy;
+		
+		if(!_.getCh.dummy) {
+			dummy = document.createElement('_');
+			
+			dummy.style.display = 'inline-block';
+		}
+		else {
+			dummy = _.getCh.dummy;
+		}
+		
+		if(dummy.parentNode !== parent) {
+			parent.appendChild(dummy);
+		}
+		
+		dummy.textContent = input.value;
+		
+		var w = dummy.offsetWidth;
+		
+		dummy.style.display = 'none';
+
+		input.ch = w/input.value.length;
+		
+		return w + 'px';
+	}
+}
+
+_.fontSize = (function(){
+	var sizes = [];
+	
+	sizes[9]  = 360;
+	sizes[11] = 340;
+	sizes[13] = 290;
+	sizes[20] = 200;
+	sizes[40] = 100;
+	
+	var lowerBound = 9;
+	
+	for(var i=0; i<9; i++) {
+		sizes[i] = sizes[9];
+	}
+	
+	for(var i=9; i<sizes.length; i++) {
+		if(sizes[i] === undefined) {
+			for(var j=i+1; sizes[j] === undefined; j++);
+			
+			var upperBound = j,
+			    range = upperBound - lowerBound,
+			    ratio = (i - lowerBound)/range;
+
+			sizes[i] = sizes[lowerBound] - ratio * (sizes[lowerBound] - sizes[upperBound])
+		}
+		else {
+			lowerBound = i;
+		}
+	}
+	
+	return function(length) {
+		if(sizes[length]) {
+			return sizes[length];
+		}
+		
+		return sizes[sizes.length - 1];
+	}
+})();
+
+_.prototype = {
+	positionIndicator: function(indicator, index, length) {
+		var ch = this.tester.ch || this.tester.offsetWidth / this.tester.value.length;
+		
+		indicator.style.left = ch * index + 'px';
+		indicator.style.left = index + 'ch';
+		indicator.style.width = ch * length + 'px';
+		indicator.style.width = length + 'ch';
+	},
+	
+	test: function() {
+		if(!this.input.value) { return; }
+		
+		try {
+			var pattern = this.pattern = RegExp(this.input.value, this.flags);
+			this.input.parentNode.parentNode.classList.remove('invalid');
+		}
+		catch(e) {
+			this.input.parentNode.parentNode.classList.add('invalid');
+			return;
+		}
+		
+		var test = this.tester.value.replace(/\\n/g, '\n').replace(/\\r/g, '\r'),
+		    isMatch = pattern.test(test);
+		
+		this.tester.parentNode.parentNode.classList[isMatch? 'remove' : 'add']('invalid');
+		
+		this.matches = [];
+		
+		pattern.lastIndex = 0;
+
+		if (isMatch) {
+			// Show exact matches
+			var match;
+			
+			while (match = pattern.exec(test)) {
+				var matches = {
+					index: match.index,
+					length: match[0].length,
+					subpatterns: match
+				};
+
+				this.matches.push(matches);
+				
+				if(matches.length === 0) {
+					pattern.lastIndex++;
+				}
+			}
+		}
+		
+		this.nextMatch();
+	},
+	
+	toggleFlag: function (flag) {
+		if (this.flags.indexOf(flag) > -1) {
+			this.flags = this.flags.replace(RegExp(flag, 'g'), '');
+		}
+		else {
+			this.flags += flag;
+		}
+		
+		this.flagsContainer.textContent = this.flags;
+		
+		this.test();
+	},
+	
+	gotoMatch: function (index) {
+		if(!this.matches.length) {
+			this.matchIndicator.style.display = 'none'; 
+			
+			this.subpatterns = [];
+		}
+		else {
+			var match = this.matches[index];
+			
+			if(match) {
+				this.positionIndicator(this.matchIndicator, match.index, match.length);
+				this.matchIndicator.style.display = ''; 
+				
+				this.subpatterns = match.subpatterns.slice() || []; // slice for cloning
+			}
+			else {
+				throw Error('No match exists at ' + index);
+			}
+		}
+		
+		this.nextSubpattern();
+	},
+	
+	gotoSubpattern: function (index) {
+		if(!this.subpatterns.length) {
+			this.submatchIndicator.style.display = 'none'; 
+		}
+		else {
+			var match = this.matches[this.matches.index],
+			    subpattern = this.subpatterns[index];
+			
+			if(match) {
+				var offset = match.index + match.subpatterns[0].indexOf(subpattern);
+				this.positionIndicator(this.submatchIndicator, offset, subpattern.length);
+				this.submatchIndicator.style.display = '';
+			}
+			else {
+				throw Error('No subpattern exists at ' + index);
+			}
+		}
+	},
+	
+	nextMatch: function () {
+		var matches = this.matches;
+
+		if(!('index' in matches) || matches.index < 0 || matches.index + 1 >= matches.length) {
+			matches.index = -1;
+		}
+		
+		this.gotoMatch(++matches.index);
+	},
+	
+	prevMatch: function () {
+		var matches = this.matches;
+		
+		if(!matches.index) {
+			matches.index = matches.length;
+		}
+		
+		this.gotoMatch(--matches.index);
+	},
+	
+	nextSubpattern: function () {
+		if(this.subpatterns === undefined) {
+			this.nextMatch();
+		}
+		
+		var matches = this.subpatterns;
+
+		if(!('index' in matches) || matches.index < 0 || matches.index + 1 >= matches.length) {
+			matches.index = -1;
+		}
+		
+		this.gotoSubpattern(++matches.index);
+	},
+	
+	prevSubpattern: function () {
+		var matches = this.subpatterns;
+		
+		if(!matches.index) {
+			matches.index = matches.length;
+		}
+		
+		this.gotoSubpattern(--matches.index);
+	}
+};
+
+})();
---
+++ regexplained.js
@@ -0,0 +1,218 @@
+// Remove spaces in syntax breakdown and add classes to the ones that are towards the end
+$$('.syntax-breakdown h1 code').forEach(function(code){
+	code.innerHTML = code.innerHTML
+		.replace(/[\t\r\n]/g, '');
+	
+	var text = code.textContent;
+	
+	$$('span', code).forEach(function(span){
+		span.classList.add('delayed');
+		
+		if(text.indexOf(span.textContent) > text.length/2) {
+			// FIXME will break when there are duplicates
+			span.classList.add('after-middle');
+		}
+	});
+});
+
+$$('.regex-test.slide').forEach(function(slide){
+	slide.tester = new RegExpTester(slide);
+});
+
+(function(){
+	var _ = window.TwitterSearch = function(q, details) {
+		this.q = q;
+		this.details = details;
+	}
+	
+	_.maxId = 0;
+	
+	_.prototype = {
+		send: function(q) {
+			window.twitterSearch = this;
+			
+			$u.script('http://search.twitter.com/search.json?' +
+				'callback=twitterSearch.callback' +
+				'&q=' + encodeURIComponent(this.q) +
+				'&since_id=' + _.maxId +
+				'&rpp=100' +
+				'&result_type=recent');
+		},
+		
+		callback: function(data) {
+			delete window.twitterSearch;
+			
+			_.maxId = +data.max_id;
+			
+			this.onload && this.onload(data);
+		}
+	};
+	
+	_.dateOffset = function (date) {
+		var seconds = Math.round((+new Date - new Date(date))/1000);
+		
+		if (seconds >= 3600) {
+			var hours = Math.round(seconds/3600);
+			return hours + ' hour' + (hours===1? '' : 's') + ' ago'
+		}
+		
+		if(seconds > 60) {
+			var minutes = Math.round(seconds/60);
+			return minutes + ' minute' + (minutes===1? '' : 's') + ' ago'
+		}
+		
+		return seconds + ' seconds ago';
+	}
+})();
+
+$$('.slide[data-type="Challenge"]').forEach(function(slide){
+	var minutes = slide.getAttribute('data-duration') || 1,
+	    duration = 60 * minutes,
+	    SVGNamespace = 'http://www.w3.org/2000/svg';
+	
+	var animate = $u.element.create({
+		tag: 'animate',
+		namespace: SVGNamespace,
+		attributes: {
+			attributeName: 'stroke-dasharray',
+			values: '0,314.1593%;314.1593%,314.1593%',
+			dur: 2*duration + 's', // No fucking idea why it needs double the time!!
+			begin: 'indefinite',
+			fill: 'freeze'
+		}
+	});
+	
+	var timer = $u.element.create({
+		tag: 'div',
+		properties: {
+			className: 'timer'
+		},
+		attributes: {
+			'data-duration': minutes
+		},
+		contents: [{
+			tag: 'svg',
+			namespace: SVGNamespace,
+			contents: {
+				tag: 'circle',
+				namespace: SVGNamespace,
+				attributes: {
+					cx: '50%',
+					cy: '50%',
+					r: '25%',
+					"stroke-dasharray": '0,314.1593%'
+				},
+				contents: animate
+			}
+		}, {
+			tag: 'button',
+			contents: 'Go!',
+			properties: {
+				type: 'button',
+				onclick: function(){
+					animate.beginElement();
+					
+					var running = true;
+					timer.classList.add('running');
+					
+					var search = new TwitterSearch('#regexplained', details);
+					
+					search.onload = function(data) {
+						// Process data
+						var results = data.results,
+						    list = $('div', search.details),
+						    summary = $('summary', search.details);
+						
+						for (var i=results.length, tweet; tweet = results[--i];) {
+							
+							// Don’t add the same tweets twice
+							// Twitter Search API is fucked
+							if($('#t' + tweet.id, list)) {
+								results.splice(i, 1);
+								continue;
+							}
+							
+							$u.element.create('article', {
+								properties: {
+									id: 't' + tweet.id
+								},
+								contents: [{
+									tag: 'img',
+									properties: {
+										src: tweet.profile_image_url
+									}
+								}, {
+									tag: 'h1',
+									contents: [{
+										tag: 'a',
+										properties: {
+											href: 'http://twitter.com/' + tweet.from_user,
+											target: '_blank'
+										},
+										contents: '@' + tweet.from_user
+									}, ' ', {
+										tag: 'a',
+										properties: {
+											href: 'http://twitter.com/' + tweet.from_user + '/status/' + tweet.id,
+											target: '_blank'
+										},
+										contents: {
+											tag: 'time',
+											attributes: {
+												datetime: (new Date(tweet.created_at)).toISOString()
+											}
+										}
+									}]
+								}, {
+									tag: 'p',
+									properties: {
+										innerHTML: tweet.text.replace(/[@#]regexplained/g, '')
+									}
+								}],
+								start: list
+							});
+						}
+						
+						// Fix relative dates
+						$$('time', list).forEach(function(time) {
+							time.textContent = TwitterSearch.dateOffset(time.getAttribute('datetime'));
+						});
+						
+						summary.innerHTML = (parseInt(summary.innerHTML) || 0) + results.length + ' tweets';
+						
+						// Schedule next fetch
+						if(running) {
+							setTimeout(function() {
+								search.send();
+							}, 5000);
+						}
+					}
+					
+					search.send();
+
+					setTimeout(function(){
+						// Time’s up!
+						running = false;
+						timer.classList.remove('running');
+						
+					}, (duration + 20) * 1000); // +20s to account for the twitter lag
+				}
+			}
+		}],
+		inside: slide
+	});
+	
+	var details = $u.element.create('details', {
+		tag: 'details',
+		properties: {
+			className: 'tweets'
+		},
+		contents: [{
+			tag: 'summary',
+			contents: '0 tweets'
+		},{
+			tag: 'div'
+		}],
+		inside: slide
+	});
+});
